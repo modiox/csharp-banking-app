be3d8a123353b52451fdd00b84b1e0bb2dd47cd2
modiox <moodyalyalak@gmail.com>
2024-03-02 14:19:34 +0300

    updated march 2

--- src/index.js
+++ src/index.js
@@ -1,35 +1,34 @@
-var Transaction = /** @class */ (function () {
-    function Transaction(amount) {
+class Transaction {
+    constructor(amount) {
         this.amount = amount;
         this.date = new Date(); //creates the date automatically
     }
-    return Transaction;
-}());
+}
 //Customer Class
-var Customer = /** @class */ (function () {
-    function Customer(name, id) {
+class Customer {
+    constructor(name, id) {
         this.name = name;
         this.id = id;
         this.transactions = [];
     }
-    Customer.prototype.getName = function () {
+    getName() {
         return this.name;
-    };
-    Customer.prototype.getId = function () {
+    }
+    getId() {
         return this.id;
-    };
-    Customer.prototype.getTransaction = function () {
+    }
+    getTransaction() {
         ///returns transaction array
         return this.transactions;
-    };
-    Customer.prototype.getBalance = function () {
-        return this.transactions.reduce(function (total, transactions) { return total + transactions.amount; }, 0);
-        //adding the zero makes it 'start' from zero,, it starts from zero by default but keep this in mind moe, you can add any starting number this way
-    };
-    Customer.prototype.addTransactions = function (amount) {
+    }
+    getBalance() {
+        return this.transactions.reduce((total, transactions) => total + transactions.amount, 0);
+        // it starts from zero by default but keep this in mind moe, you can add any starting number this way
+    }
+    addTransactions(amount) {
         //create a transaction by initializing an object
-        var trans = new Transaction(amount);
-        var result = this.transactions.push(trans);
+        const trans = new Transaction(amount);
+        const result = this.transactions.push(trans);
         if (result > 0) {
             return true;
         }
@@ -37,71 +36,69 @@ var Customer = /** @class */ (function () {
             return false;
         }
         //or simpley use -> return result > 0 ? true : false;
-    };
-    return Customer;
-}());
+    }
+}
 //creating a customer and add it to transaction()
-var c1 = new Customer("moe", 404);
+const c1 = new Customer("moe", 404);
 c1.addTransactions(550);
 //Branch class
-var Branch = /** @class */ (function () {
-    function Branch(name) {
+class Branch {
+    constructor(name) {
         this.name = name;
         this.customers = [];
     }
-    Branch.prototype.getBranchName = function (name) {
+    getBranchName(name) {
         return this.name;
-    };
-    Branch.prototype.getCustomers = function () {
+    }
+    getCustomers() {
         return this.customers;
-    };
-    Branch.prototype.addCustomer = function (customer) {
+    }
+    addCustomer(customer) {
         //class Customer is the type here
         if (!this.customers.includes(customer)) {
-            var result = this.customers.push(customer);
+            const result = this.customers.push(customer);
             return result > 0 ? true : false;
         }
-    };
-    Branch.prototype.addCustomerTransaction = function (customerID, amount) {
-        var customer = this.customers.find(function (customer) { return customer.id === customerID; });
+    }
+    addCustomerTransaction(customerID, amount) {
+        const customer = this.customers.find((customer) => customer.id === customerID);
         if (customer) {
             customer.addTransactions(amount);
             return true;
         }
         else
             return false;
-    };
-    return Branch;
-}());
-var Bank = /** @class */ (function () {
-    function Bank(name) {
+    }
+}
+class Bank {
+    constructor(name) {
         this.name = name;
         this.branches = [];
     }
-    Bank.prototype.addBranch = function (branch) {
+    addBranch(branch) {
         //the type is Branch class
         // Description: Adds the branch to the branches array. Each branch should only be added once.
         if (!this.branches.includes(branch)) {
-            var result = this.branches.push(branch);
+            const result = this.branches.push(branch);
             return true;
         }
         else
             return false;
-    };
-    Bank.prototype.addCustomer = function (branch, customer) {
+    }
+    addCustomer(branch, customer) {
         if (this.branches.includes(branch)) {
-            var result = branch.addCustomer(customer);
+            const result = branch.addCustomer(customer);
             if (result) {
                 return true;
             }
         }
         return false;
-    };
-    Bank.prototype.findBranchByName = function (branchName) {
-        return this.branches.find(function (branch) { return branch.name === branchName; });
-    };
-    Bank.prototype.addCustomerTransaction = function (branch, customerId, amount) {
-        var targetBranch = this.findBranchByName(branch.name);
+    }
+    findBranchByName(branchName) {
+        return this.branches.find((branch) => branch.name === branchName);
+    }
+    addCustomerTransaction(branch, customerId, amount) {
+        const targetBranch = this.findBranchByName(branch.name);
         if (targetBranch) {
             targetBranch.addCustomerTransaction(customerId, amount);
             return true;
@@ -109,48 +106,44 @@ var Bank = /** @class */ (function () {
         else {
             return false;
         }
-    };
-    Bank.prototype.checkBranch = function (branch) {
+    }
+    checkBranch(branch) {
         return this.branches.includes(branch);
-    };
-    Bank.prototype.listCustomers = function (branch, includeTransactions) {
-        var branchCustomers = branch.getCustomers();
-        var customerList = [];
+    }
+    listCustomers(branch, includeTransactions) {
+        const branchCustomers = branch.getCustomers();
+        const customerList = [];
         // Iterate through customers of the specified branch
-        for (var _i = 0, branchCustomers_1 = branchCustomers; _i < branchCustomers_1.length; _i++) {
-            var customer = branchCustomers_1[_i];
-            var customerInfo = "Name: ".concat(customer.getName(), ", ID: ").concat(customer.getId());
+        for (const customer of branchCustomers) {
+            let customerInfo = `Name: ${customer.getName()}, ID: ${customer.getId()}`;
             if (includeTransactions) {
                 // Include transactions if specified
-                var transactions = customer.getTransaction();
-                var transactionAmounts = transactions.map(function (transaction) { return transaction.amount; });
-                var totalBalance = transactions.reduce(function (total, transaction) { return total + transaction.amount; }, 0);
-                customerInfo += ", Transactions: ".concat(transactionAmounts.join(", "), ", Total Balance: ").concat(totalBalance);
+                const transactions = customer.getTransaction();
+                const transactionAmounts = transactions.map((transaction) => transaction.amount);
+                const totalBalance = transactions.reduce((total, transaction) => total + transaction.amount, 0);
+                customerInfo += `, Transactions: ${transactionAmounts.join(", ")}, Total Balance: ${totalBalance}`;
             }
             customerList.push(customerInfo);
         }
         return customerList;
-    };
-    return Bank;
-}());
-var arizonaBank = new Bank("Arizona");
-var westBranch = new Branch("West Branch");
-var sunBranch = new Branch("Sun Branch");
-var customer1 = new Customer("John", 1);
-var customer2 = new Customer("Anna", 2);
-var customer3 = new Customer("John", 3);
-arizonaBank.addBranch(sunBranch);
+    }
+}
+const arizonaBank = new Bank("Arizona");
+const westBranch = new Branch("West Branch");
+const sunBranch = new Branch("Sun Branch");
+const customer1 = new Customer("John", 1);
+const customer2 = new Customer("Anna", 2);
+const customer3 = new Customer("John", 3);
 arizonaBank.addBranch(westBranch);
-// arizonaBank.findBranchByName("bank");
-// arizonaBank.findBranchByName("sun");
+arizonaBank.addBranch(sunBranch);
 arizonaBank.addCustomer(westBranch, customer1);
 arizonaBank.addCustomer(westBranch, customer3);
 arizonaBank.addCustomer(sunBranch, customer1);
 arizonaBank.addCustomer(sunBranch, customer2);
 arizonaBank.addCustomerTransaction(westBranch, customer1.getId(), 3000);
 arizonaBank.addCustomerTransaction(westBranch, customer1.getId(), 2000);
-arizonaBank.addCustomerTransaction(westBranch, customer2.getId(), 3000);
-customer1.addTransactions(1000);
+arizonaBank.addCustomerTransaction(sunBranch, customer2.getId(), 3000);
+customer1.addTransactions(-1000);
 console.log(customer1.getBalance());
 console.log(arizonaBank.listCustomers(westBranch, true));
 console.log(arizonaBank.listCustomers(sunBranch, true));
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 class Transaction {
   amount: number;
   date: Date;
-  constructor(amount: number) {
+  constructor(amount:number) {
     this.amount = amount;
     this.date = new Date(); //creates the date automatically
   }
@@ -169,11 +169,8 @@ const customer1 = new Customer("John", 1);
 const customer2 = new Customer("Anna", 2);
 const customer3 = new Customer("John", 3);
 
-arizonaBank.addBranch(sunBranch);
 arizonaBank.addBranch(westBranch);
-
-// arizonaBank.findBranchByName("bank");
-// arizonaBank.findBranchByName("sun");
+arizonaBank.addBranch(sunBranch);
 
 arizonaBank.addCustomer(westBranch, customer1);
 arizonaBank.addCustomer(westBranch, customer3);
@@ -182,9 +179,10 @@ arizonaBank.addCustomer(sunBranch, customer2);
 
 arizonaBank.addCustomerTransaction(westBranch, customer1.getId(), 3000);
 arizonaBank.addCustomerTransaction(westBranch, customer1.getId(), 2000);
-arizonaBank.addCustomerTransaction(westBranch, customer2.getId(), 3000);
+arizonaBank.addCustomerTransaction(sunBranch, customer2.getId(), 3000);
 
 customer1.addTransactions(1000);
+
 console.log(customer1.getBalance());
 console.log(arizonaBank.listCustomers(westBranch, true));
 console.log(arizonaBank.listCustomers(sunBranch, true));
